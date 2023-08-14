affd287d1314d738345ab6decee868facb980740
huijunam <aaa3k5@naver.com>
2023-08-14 19:33:54 +0900

    ✨

--- user_app/src/App.js
+++ user_app/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) =>{
+    setUsersList((prevUsersList) =>{
+      return [...prevUsersList, {name: username, age: age, id: Math.random().toString() }];
+    });
+  }
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser= {addUserHandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
