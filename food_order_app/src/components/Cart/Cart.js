d75906aabfd6de8d4e243cf0c566fcc65d51eac8
huijunam <aaa3k5@naver.com>
2023-08-17 11:07:46 +0900

    ✨feat: 	새로운 기능 추가 - add Modal.js, Cart.js

---
+++ food_order_app/src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "m1",
+          name: "Sushi",
+          amount: 2,
+          price: 22.99,
+        },
+      ].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
+
